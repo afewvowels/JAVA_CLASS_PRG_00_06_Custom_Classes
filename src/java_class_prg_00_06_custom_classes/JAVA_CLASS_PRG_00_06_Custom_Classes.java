09bd0051b787dc0c3b7a95eb704f23cae8c8a7e3
kbsmith01 <kbsmith01@TS15849.ysu.local>
2018-01-26 12:05:15 -0500

    Initial application created.

---
+++ src/java_class_prg_00_06_custom_classes/JAVA_CLASS_PRG_00_06_Custom_Classes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_class_prg_00_06_custom_classes;
+
+/**
+ *
+ * @author kbsmith01
+ */
+public class JAVA_CLASS_PRG_00_06_Custom_Classes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
