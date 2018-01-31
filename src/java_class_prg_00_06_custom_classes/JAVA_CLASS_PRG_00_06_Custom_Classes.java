3c0f924842faf451cafdffc17a6a41f528ec5355
kbsmith01 <kbsmith01@TS15849.ysu.local>
2018-01-31 12:51:24 -0500

    Class over.

--- src/java_class_prg_00_06_custom_classes/JAVA_CLASS_PRG_00_06_Custom_Classes.java
+++ src/java_class_prg_00_06_custom_classes/JAVA_CLASS_PRG_00_06_Custom_Classes.java
@@ -15,7 +15,40 @@ public class JAVA_CLASS_PRG_00_06_Custom_Classes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Clock clockA = new Clock();
+        
+        clockA.setHour(11);
+        clockA.setMinute(50);
+        
+        
+        Clock clockB = new Clock();
+        
+        clockB.setHour(23);
+        clockB.setMinute(55);
+        
+        for(int i = 0 ; i < 20 ; i++) {
+            clockA.incrementMinute();
+//            System.out.println(clockA.toString());
+            System.out.println("Clock A: " + clockA.getHour() + ":" + clockA.getMinute());
+        }
+        
+        for(int i = 0 ; i < 20 ; i++) {
+            clockB.incrementMinute();
+            System.out.println("Clock B: " + clockB.getHour() + ":" + clockB.getMinute());
+        }
+        
+        // Aggregation example, creates time zone clocks by
+        // incrementing each clock in the array by one hour
+        final int ARR_SIZE = 5;
+        
+        Clock[] clocks = new Clock[ARR_SIZE];
+        
+        for(int i = 0 ; i < clocks.length; i++) {
+            clocks[i] = new Clock(12 + i, 55);
+            System.out.println("\nClock " + (i + 1));
+//            System.out.println(clocks[i].getHour() + ":" + clocks[i].getMinute());
+            System.out.println(clocks[i].toString());
+        }
     }
     
 }
